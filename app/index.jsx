77c33dc84a957cc2237073836a3f995658076a9d
szeyui <e1156818@u.nus.edu>
2024-05-23 20:17:36 +0800

    goo HOME

--- app/index.jsx
+++ app/index.jsx
@@ -6,9 +6,9 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{fontSize:45, fontFamily: "Poppins-Black"}}>Mars Change</Text>
+      <Text style={{fontSize:45, fontFamily: "Poppins-Black"}}>EARTH SWAP</Text>
       <StatusBar style="auto" />
-      <Link href ="/Home" style ={{color:'black'}}>PROCEED Home</Link>
+      <Link href ="/Home" style ={{color:'black'}}>gooo HOME</Link>
     </View>
   );
 }
